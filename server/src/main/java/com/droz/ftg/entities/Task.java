523b665627528b32b7fd8064fc0925d671304934
Dima Rassin <dima.rassin@gmail.com>
2012-01-07 03:22:55 +0200

    creating project...

---
+++ server/src/main/java/com/droz/ftg/entities/Task.java
@@ -0,0 +1,12 @@
+package com.droz.ftg.entities;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Date: 1/7/12
+ *
+ * @author Dima Rassin
+ */
+@XStreamAlias("task")
+public class Task {
+}
